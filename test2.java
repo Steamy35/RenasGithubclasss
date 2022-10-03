ea45038cbffc51e068c9dfe97359fa8c83553498
Steamy35 <114784066+Steamy35@users.noreply.github.com>
2022-10-03 19:54:21 -0400

    first commit

--- test2.java
+++ test2.java
@@ -2,5 +2,6 @@ public class test2 {
     public static void main(String[] args) {
         System.out.println("gebes kamlumbaga");
         System.out.println("gebes");
+        System.out.println("kaplumbaga");
     }
 }
